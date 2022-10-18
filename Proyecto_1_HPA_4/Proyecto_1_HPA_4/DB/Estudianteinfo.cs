292b3453c7bdc01310b5d773a49762effcd6f19b
Eric Mason <57864479+ericMaxon@users.noreply.github.com>
2022-10-17 22:02:34 -0500

    Inicializar en el constructor

--- Proyecto_1_HPA_4/Proyecto_1_HPA_4/DB/Estudianteinfo.cs
+++ Proyecto_1_HPA_4/Proyecto_1_HPA_4/DB/Estudianteinfo.cs
@@ -7,25 +7,27 @@ namespace Proyecto_1_HPA_4.DB
 {
     class Estudianteinfo
     {
-        public List<Estudiante> ListadodeEstudiantes = new List<Estudiante>
-        {
-            new Estudiante
-            {
-                Nombre="Diego Casto",
-                Cedula="1-1234-12345",
-                Fecha=DateTime.Now.ToString()
-            },
-            new Estudiante
-            {
-                Nombre="Pedro Jaramillo",
-                Cedula="1-1234-12346",
-                Fecha=DateTime.Now.ToString()
-            }
-        };
+        public List<Estudiante> ListadodeEstudiantes;
+
         public Estudianteinfo()
         {
-
+            ListadodeEstudiantes = new List<Estudiante>
+            {
+                new Estudiante
+                {
+                    Nombre="Diego Casto",
+                    Cedula="1-1234-12345",
+                    Fecha=DateTime.Now.ToString()
+                },
+                new Estudiante
+                {
+                    Nombre="Pedro Jaramillo",
+                    Cedula="1-1234-12346",
+                    Fecha=DateTime.Now.ToString()
+                }
+            };
         }
+
         public void AgregarEstudiante(Estudiante estudiante)
         {
             ListadodeEstudiantes.Add(estudiante);
